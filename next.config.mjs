b195e7de18c6b04f53c0fef4ca2b4f79f3ce0159
DmitriyKu <44435597+DmitriyKu@users.noreply.github.com>
2026-05-31 23:23:11 +0200

    Prepare static FTP export config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   poweredByHeader: false,
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     formats: ['image/avif', 'image/webp'],
   },
 }
